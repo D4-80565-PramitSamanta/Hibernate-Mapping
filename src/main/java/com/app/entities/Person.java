2eb5263fa94866bb22213e357da7f716381852e6
D4-80565-PramitSamanta <63332503+D4-80565-PramitSamanta@users.noreply.github.com>
2024-02-19 11:25:39 +0530

    Person entity class added

---
+++ src/main/java/com/app/entities/Person.java
@@ -0,0 +1,19 @@
+package com.app.entities;
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class Person {
+	String name;
+	String address;
+	
+}
